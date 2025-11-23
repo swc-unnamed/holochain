f49bc052b0d9bbc6528e6bf35275c7d248650822
swc-marc <marcinius@protonmail.com>
2025-11-22 22:44:14 -0500

    fix: build output

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
     }
   },
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+      out: 'build'
+    }),
     experimental: {
       remoteFunctions: true
     },
